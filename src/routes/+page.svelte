c2f1af668f8e4faf2ef6cdf3d35345c331283106
Ryan Atkinson <mail@ryanatkn.com>
2023-02-20 21:46:29 -0700

    hello world

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
-	import Mreows from '$lib/Mreows.svelte';
+	import {Portal} from '@feltcoop/dealt';
 
-	let mreows: Array<{icon: string}> | undefined;
+	import SsagScene from '$routes/SsagScene.svelte';
 </script>
 
-<main class="markup column">
-	<section>
-		<header class="centered-hz">
-			<h1><a href="https://github.com/ryanatkn/ssag">ssag</a></h1>
-		</header>
-	</section>
-	<Mreows bind:mreows />
+<main>
+	<Portal>
+		<SsagScene />
+	</Portal>
 </main>
 
 <style>
 	main {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		margin: 0 auto;
-	}
-	h1 {
-		text-align: center;
+		position: absolute;
+		inset: 0;
 	}
 </style>
---
+++ src/routes/SsagScene.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import {onDestroy, onMount} from 'svelte';
+	import {writable} from 'svelte/store';
+	import {
+		World,
+		setViewport,
+		SurfaceWithController,
+		getPixi,
+		createCameraStore,
+		getLayout,
+		Stage,
+		type ExitStage,
+	} from '@feltcoop/dealt';
+
+	import {Stage0} from '$routes/stage0';
+
+	export let pixi = getPixi();
+	export let layout = getLayout();
+
+	let viewportSize = Math.min($layout.width, $layout.height);
+	$: viewportSize = Math.min($layout.width, $layout.height);
+
+	const WORLD_SIZE = 256;
+
+	// Scenes control the `viewport` and `camera`.
+	const viewport = setViewport(writable({width: viewportSize, height: viewportSize}));
+	$: viewport.set({width: viewportSize, height: viewportSize});
+	const camera = createCameraStore();
+	$: camera.setDimensions(WORLD_SIZE, WORLD_SIZE, $viewport.width, $viewport.height);
+
+	let stage: Stage | undefined | null;
+	let settingUp: boolean | undefined;
+
+	const exit: ExitStage = (outcome) => {
+		console.log(`exit outcome`, outcome);
+		createStage();
+	};
+
+	const createStage = () => {
+		if (settingUp) return;
+		settingUp = true;
+		if (stage) destroyStage();
+		stage = new Stage0({exit, camera, viewport, layout});
+		void stage.setup({stageStates: []});
+		settingUp = false;
+	};
+
+	onMount(() => {
+		createStage();
+	});
+
+	// TODO abstract this
+	const destroyStage = () => {
+		if (!stage) return;
+		console.log(`disposing stage`, stage);
+		stage.dispose();
+		stage = null;
+	};
+	onDestroy(destroyStage);
+</script>
+
+{#if stage}
+	{#key stage}
+		<World {stage} {pixi} />
+		<SurfaceWithController controller={stage.controller} />
+	{/key}
+{/if}
